7825b158b589b56b425bceeb5f7787f322d31cf0
mrohtsalu <104059019+mrohtsalu@users.noreply.github.com>
2022-04-22 22:27:02 +0300

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -29,3 +29,12 @@ async function addDistrictsGeoJson(url) {
  polygons.addTo(map)
 }
 addDistrictsGeoJson('geojson/tartu_city_districts_edu.geojson')
+
+// add geoJSON points layer*
+async function addCelltowersGeoJson(url) {
+ const response = await fetch(url)
+ const data = await response.json()
+ const markers = L.geoJson(data)
+ markers.addTo(map)
+}
+addCelltowersGeoJson('geojson/tartu_city_celltowers_edu.geojson')
