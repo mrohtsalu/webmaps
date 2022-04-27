8c78c3c4032a692d9424b30cbbe74f246477a599
mrohtsalu <104059019+mrohtsalu@users.noreply.github.com>
2022-04-27 11:00:05 +0300

    Update heatmap.js

--- js/heatmap.js
+++ js/heatmap.js
@@ -6,6 +6,15 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 })
 osm.addTo(map)
 
+// add geoJSON polygons layer*
+async function addDistrictsGeoJson(url) {
+ const response = await fetch(url)
+ const data = await response.json()
+ const polygons = L.geoJson(data)
+ polygons.addTo(map)
+}
+addDistrictsGeoJson('geojson/tartu_city_districts_edu.geojson')
+
 addGeoJson('geojson/tartu_city_celltowers_edu.geojson')
 // add geoJSON layer
 async function addGeoJson(url) {
