5d53d2b4a54a66437581f8e0d8ab875c687fb0bb
mrohtsalu <104059019+mrohtsalu@users.noreply.github.com>
2022-05-01 14:35:16 +0300

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -19,6 +19,32 @@ addDistrictsGeoJson('geojson/tartu_city_districts_edu.geojson')
 function popUPinfo(feature, layer) {
  layer.bindPopup(feature.properties.NIMI)
 }
+
+// get color from feature property
+function getColor(property) {
+ switch (property) {
+ case 1:
+ return '#ff0000'
+ case 13:
+ return '#009933'
+ case 6:
+ return '#0000ff'
+ case 7:
+ return '#ff0066'
+ default:
+ return '#ffffff'
+   
+// polygon style
+function polygonStyle(feature) {
+ return {
+ fillColor: getColor(feature.properties.OBJECTID),
+ fillOpacity: 0.5,
+ weight: 1,
+ opacity: 1,
+ color: 'grey',
+ }
+}
+
 // add geoJSON polygons layer
 async function addDistrictsGeoJson(url) {
  const response = await fetch(url)
